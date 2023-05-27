a6e47a0c8eda4aa2436c90cce72e2dd79c82c7f9
ashu24998 <ashutosh24998@gmail.com>
2023-05-27 18:38:10 +0530

    Minor Bug Fixes

--- app.js
+++ app.js
@@ -2,59 +2,52 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const date = require(__dirname + "/date.js");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.set('strictQuery', false);
+mongoose.set("strictQuery", false);
 // mongoose.connect("mongodb://localhost:27017/todolistDB");
-mongoose.connect("mongodb+srv://ashu24998:<myPassword>@cluster0.xgfoy2i.mongodb.net/todolistDB");
+// mongoose.connect("mongodb+srv://ashu24998:<myPassword>@cluster0.xgfoy2i.mongodb.net/todolistDB");
+mongoose.connect(process.env.DB_URL);
 
 const itemsSchema = new mongoose.Schema({
-  name: String
+  name: String,
 });
 
 const ItemModel = mongoose.model("Item", itemsSchema);
 
 const item1 = new ItemModel({
-  name: "Get Up"
-})
+  name: "Get Up",
+});
 
-const item2 = new ItemModel({
-  name: "Brush Teeth"
-})
+const defaultArray = [item1];
 
-const item3 = new ItemModel({
-  name: "Breakfast"
-})
-
-const defaultArray = [item1, item2, item3];
-
-app.get("/", function(req, res) {
-
-  ItemModel.find(function(err, items) {
-    if(items.length === 0) {
-      ItemModel.insertMany(defaultArray, function(err) {
-        if(err) {
+app.get("/", function (req, res) {
+  ItemModel.find(function (err, items) {
+    if (items.length === 0) {
+      ItemModel.insertMany(defaultArray, function (err) {
+        if (err) {
           console.log(err);
         } else {
           console.log("Success");
         }
-      })
+      });
       res.redirect("/");
     } else {
-        const day = date.getDate();
-        res.render("list", {listTitle: day, newListItems: items});
-      }
-    })
+      const day = date.getDate();
+      res.render("list", { listTitle: day, newListItems: items });
+    }
   });
+});
 
-app.post("/", function(req, res){
-
+app.post("/", function (req, res) {
   const item = req.body.newItem;
 
   if (req.body.list === "Work") {
@@ -62,23 +55,23 @@ app.post("/", function(req, res){
     res.redirect("/work");
   } else {
     const nextItem = new ItemModel({
-      name: item
-    })
+      name: item,
+    });
     nextItem.save();
     res.redirect("/");
   }
 });
 
-app.post("/delete", function(req, res){
+app.post("/delete", function (req, res) {
   const checkedItem = req.body.checkbox;
-  ItemModel.findByIdAndDelete(checkedItem, function(err) {
-    if(err) {
+  ItemModel.findByIdAndDelete(checkedItem, function (err) {
+    if (err) {
       console.log(err);
     } else {
       console.log("Successfully deleted.");
       res.redirect("/");
     }
-  })
+  });
 });
 
 // app.get("/:path", function(req, res) {
@@ -95,10 +88,10 @@ app.post("/delete", function(req, res){
 //   });
 // });
 
-app.get("/about", function(req, res){
+app.get("/about", function (req, res) {
   res.render("about");
 });
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
